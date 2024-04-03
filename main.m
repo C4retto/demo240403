8716a3ca77d9a68353a712cb9e5089ff24e2a145
C4retto <gamma0925@163.com>
2024-04-03 20:47:26 +0800

    r

--- main.m
+++ main.m
@@ -6,15 +6,16 @@ function main
     maxgen = 100;
     popsize = 102;
     pm = 0.15;
-    pc = 0.1:0.1:1;
-    pc2 = 0.1:0.1:1;
+    pc = 0.1:0.2:0.9;
+    pc2 = 0.1:0.2:0.9;
 
     p = testnetwork(pname);
     runTime = 10;
     for cc = pc
     for cc2 = pc2
         disp([cc,cc2]);
-        result = zeros(4,runTime);
+        %result = zeros(4,runTime);
+        result = zeros(1,runTime);
         for runtime = 1:runTime
             pop = demo(p,popsize,maxgen,pm,cc,cc2,NetEmb);
             modular = zeros(1,popsize);
@@ -22,9 +23,9 @@ function main
                 modular(1,ii) = modularity(p.adj, Decode(pop(ii,:)));
             end
             result(1,runtime) = min(modular);
-            result(2,runtime) = max(modular);
-            result(3,runtime) = std(modular);
-            result(4,runtime) = mean(modular);
+            %result(2,runtime) = max(modular);
+            %result(3,runtime) = std(modular);
+            %result(4,runtime) = mean(modular);
         end
         disp(result);
     end
